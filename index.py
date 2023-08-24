c9a641cdf1a096626229510664b5e52e92fad60d
Sefora Rodriguez <seforarodriguez13@gmail.com>
2023-08-24 18:23:05 -0400

    added a str method to stringify my answers for the llama, still need to do it to the other animals

--- index.py
+++ index.py
@@ -1,3 +1,7 @@
 from swimming import Goose, Crocodile, Hippopotamus, Swan, RatSnakes, Flamingos, Copperheads
 from slithering import Mallards, Goldfish, Clownfish, Starfish
 from walking import Llama, Donkeys, Unicorns, Goats
+
+miss_fuzz = Llama("Miss Fuzz", "domestic llama", "morning", "Llama Chow" )
+
+print(miss_fuzz)
--- slithering/slithering.py
+++ slithering/slithering.py
@@ -3,41 +3,57 @@ from datetime import date
 
 class Mallards:
 
-    def __init__(self, name, species):
+    def __init__(self, name, species, food):
         # Establish the properties of each animal
         # with a default value
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.slithering = True
+        self.food = food
+
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
         
 
 class Goldfish:
 
-    def __init__(self, name, species):
+    def __init__(self, name, species, food):
         # Establish the properties of each animal
         # with a default value
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.slithering = True
+        self.food = food
+
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
 
 class Clownfish:
 
-    def __init__(self, name, species):
+    def __init__(self, name, species, food):
         # Establish the properties of each animal
         # with a default value
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.slithering = True
+        self.food = food
+
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
 
 class Starfish:
 
-    def __init__(self, name, species):
+    def __init__(self, name, species, food):
         # Establish the properties of each animal
         # with a default value
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.slithering = True
+        self.food = food
+
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
