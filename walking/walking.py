c3df2a5eb52731140aefdc6074dee9d182cdc9db
Sefora Rodriguez <seforarodriguez13@gmail.com>
2023-08-24 22:15:35 -0400

    created new classes for the different attractions, inserted the equivalent to loaddata and printed the data with different forloops again

--- walking/walking.py
+++ walking/walking.py
@@ -64,3 +64,16 @@ class Goats:
 
     def feed(self):
         print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
+
+class Burro:
+
+    def __init__(self, name, species, shift, food):
+        self.name = name
+        self.species = species
+        self.shift = shift
+        self.date_added = date.today()
+        self.walking = True
+        self.food = food
+    
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
