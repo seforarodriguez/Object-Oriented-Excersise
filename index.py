7bf6c7528252d89c727e9a0453e673ba246236b0
Sefora Rodriguez <seforarodriguez13@gmail.com>
2023-09-03 17:05:52 -0400

    added the refactoring, still some bugs to fix with a recursion with the iteration of the animals in the snakeru attraction

--- index.py
+++ index.py
@@ -34,48 +34,51 @@ varmint_village.adding_animals_to_attraction(miss_furry)
 varmint_village.adding_animals_to_attraction(mr_fuzzy)
 varmint_village.adding_animals_to_attraction(ms_donke)
 
-# print(varmint_village.last_critter_added)
-# snakeru = SnakePit("Snakeru Playground", "The best place to get bitten")
-# snakeru.adding_animals_to_attraction(Freida)
-# snakeru.adding_animals_to_attraction(Lola)
-# snakeru.adding_animals_to_attraction(miss_carla)
-# snakeru.adding_animals_to_attraction(miss_fuzzy)
-# snakeru.adding_animals_to_attraction(mr_Carlo)
-# snakeru.adding_animals_to_attraction(breakeba)
-# snakeru.adding_animals_to_attraction(Furr_man)
+print(varmint_village.last_critter_added)
 
-# wettingland = Wetlands("Ocean Land", "Best place to play in the Summer")
-# wettingland.adding_animals_to_attraction(miss_fuzzle)
-# wettingland.adding_animals_to_attraction(stinky)
-# wettingland.adding_animals_to_attraction(stanky)
-# wettingland.adding_animals_to_attraction(kanky)
+snakeru = SnakePit("Snakeru Playground", "The best place to get bitten")
+snakeru.adding_animals_to_attraction(Lola)
+snakeru.adding_animals_to_attraction(miss_carla)
+snakeru.adding_animals_to_attraction(miss_fuzzy)
+snakeru.adding_animals_to_attraction(mr_Carlo)
+snakeru.adding_animals_to_attraction(breakeba)
+snakeru.adding_animals_to_attraction(Furr_man)
+snakeru.adding_animals_to_attraction(Freida)
 
+wettingland = Wetlands("Ocean Land", "Best place to play in the Summer")
+wettingland.adding_animals_to_attraction(miss_fuzzle)
+wettingland.adding_animals_to_attraction(stinky)
+wettingland.adding_animals_to_attraction(stanky)
+wettingland.adding_animals_to_attraction(kanky)
+
+
+
+
+for animal in varmint_village.animals:
+    print(f'{varmint_village.attraction_name} is where you will find animals like {animal.name} the {animal.species} in {varmint_village.attraction_name}')
+
+#this is printing all the animals per attraction and showing it in the console with the iteration
+print(f"{varmint_village.attraction_name} is where you will find amazing animals that you can pet such as:")
+for animal in varmint_village.animals:
+    print(animal)
+
+print(f"{snakeru.attraction_name} is where you will find amazing animals that you can pet such as:")
+for animal in snakeru.animals:
+    print(animal)
+    
+print(f"{wettingland.attraction_name} is where you will find amazing animals that you can pet such as:")
+for animal in wettingland.animals:
+    print(animal)
+
+
+print(miss_fuzz.chip_number)
+
+print("These are the Methods and different examples of the Goose Class")
 Freida.run()
 Freida.swim()
 
+# This code is iterating through the methods in the Goose Class
 methods_in_class = dir(Freida)
 
 for method in methods_in_class:
     print(method)
-
-
-
-# for animal in varmint_village.animals:
-#     print(f'{varmint_village.attraction_name} is where you will find animals like {animal.name} the {animal.species} in {varmint_village.attraction_name}')
-
-# #this is printing all the animals per attraction and showing it in the console with the iteration
-# print(f"{varmint_village.attraction_name} is where you will find amazing animals that you can pet such as:")
-# for animal in varmint_village.animals:
-#     print(f' * {animal.name} the {animal.species} ')
-
-# print(f"{snakeru.attraction_name} is where you will find amazing animals that you can pet such as:")
-# for animal in snakeru.animals:
-#     print(f' * {animal.name} the {animal.species}')
-    
-# print(f"{wettingland.attraction_name} is where you will find amazing animals that you can pet such as:")
-# for animal in wettingland.animals:
-#     print(f' * {animal.name} the {animal.species}')
-
-# miss_fuzz.chip_number = 99999
-
-# print(miss_fuzz.chip_number)
